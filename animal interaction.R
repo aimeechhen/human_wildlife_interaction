5af2fdba8fe9db6fdfcd791198cdb1f2d8649b4a
Kat Chhen <105258138+katchhen@users.noreply.github.com>
2023-02-05 20:00:10 -0800

    setting up the project

---
+++ animal interaction.R
@@ -0,0 +1,7 @@
+library("readr")
+
+DATA <- read_csv("C:/Users/achhen/OneDrive - UBC/BIOL 520C Statistical Modelling/Term Paper research project/pca-human-wildlife-coexistence-animals-involved-detailed-records-2010-2021.csv")
+# DATA <- read_csv("https://open.canada.ca/data/en/datastore/dump/b2a9f7e4-7c49-471d-8337-0192c15dd52a?bom=True")
+  # REFERENCE: https://open.canada.ca/data/en/dataset/cc5ea139-c628-46dc-ac55-a5b3351b7fdf/resource/b2a9f7e4-7c49-471d-8337-0192c15dd52a?inner_span=True
+
+
